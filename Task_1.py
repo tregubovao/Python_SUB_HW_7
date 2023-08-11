f7be8056f974fb816c7bc194c532caa6f7449ffc
Aleksandr <tregubovao@mail.ru>
2023-08-11 13:45:42 +0300

    HW_7_ver3

--- Task_1.py
+++ Task_1.py
@@ -21,11 +21,12 @@ def filename_gen(min_len_name: int, max_len_name: int) -> str:
     return res_word
 
 def create_file(ext: str,
+            quantity_files: int = 42,
             min_len_name: int = 6, 
             max_len_name: int = 30,
             min_quant_bytes: int = 256,
             max_quant_bytes: int = 4096, 
-            quantity_files: int = 42):
+            ):
     
     for _ in range(quantity_files):
         with open(filename_gen(min_len_name, max_len_name) + ext, 'wb') as f:
@@ -39,7 +40,7 @@ max_quant_bytes = 16
 quantity_files = 3
 
 if __name__ == '__main__':
-    create_file(ext, min_len_name, 
-         max_len_name, min_quant_bytes, 
-         max_quant_bytes, quantity_files)
+    create_file(ext, quantity_files, 
+            min_len_name, max_len_name, 
+            min_quant_bytes, max_quant_bytes)
 
